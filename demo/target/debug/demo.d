3dc627366671734af11363177fb8b43ef40a6cc0
Emison <emison@EmisondeMacBook-Pro-2.local>
2021-07-22 11:13:57 +0800

    upload basic file

---
+++ demo/target/debug/demo.d
@@ -0,0 +1,1 @@
+/Users/emison/Desktop/Verdictd_OPA/demo/target/debug/demo: /Users/emison/Desktop/Verdictd_OPA/demo/src/main.rs
---
+++ demo/target/debug/deps/demo.d
@@ -0,0 +1,5 @@
+/Users/emison/Desktop/Verdictd_OPA/demo/target/debug/deps/demo: src/main.rs
+
+/Users/emison/Desktop/Verdictd_OPA/demo/target/debug/deps/demo.d: src/main.rs
+
+src/main.rs:
